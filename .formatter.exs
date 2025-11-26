b94bb9091ba6fec748d4b04999678c17399cd3c6
Bradley Golden <golden.bradley@gmail.com>
2025-11-26 06:06:09 -0600

    Add formatter config to export locals_without_parens for DSL
    Configure Spark.Formatter and export locals_without_parens so consuming
    projects can format DSL keywords without parentheses when they import
    from ash_agent_studio.

--- .formatter.exs
+++ .formatter.exs
@@ -1,3 +1,30 @@
+# Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  plugins: [Spark.Formatter],
+  import_deps: [:ash, :spark, :ash_agent],
+  locals_without_parens: [
+    # AshAgentStudio DSL - agent_studio section
+    label: 1,
+    description: 1,
+    group: 1,
+    redact_fields: 1,
+
+    # AshAgentStudio DSL - input entity
+    input: 1,
+    input: 2
+  ],
+  export: [
+    locals_without_parens: [
+      # AshAgentStudio DSL - agent_studio section
+      label: 1,
+      description: 1,
+      group: 1,
+      redact_fields: 1,
+
+      # AshAgentStudio DSL - input entity
+      input: 1,
+      input: 2
+    ]
+  ]
 ]
